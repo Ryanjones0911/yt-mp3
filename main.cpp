61a50b42e444b71edbf6ab8afaf97fe6f5609f58
Ryan <65054422+Ryanjones0911@users.noreply.github.com>
2024-12-25 15:33:39 -0600

    directory management

--- main.cpp
+++ main.cpp
@@ -2,22 +2,27 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-
-//attempt at making a custom youtube to mp3 command line tool
-//
-// 1) download a supplied youtube video (link given in command line) as a mp4
-// 2) use ffmpeg to convert mp4 to .wav (maybe option to specify file type later on)
-// in theory that's all you need
+#include <filesystem>
+#include <fstream>
 
 
 
 int main (int argc, char *argv[]) 
 {
+
+  //create directory 'media' if it does not already exist. This is where all converted audio files will go
+  std::filesystem::path mediaPath = "/Users/ryan/convertedMedia";
+  if (!exists(mediaPath)) 
+  {
+    std::filesystem::create_directory(mediaPath);
+    printf("Directory created\n");
+  }
+  
   std::string url = argv[1];
   std::string title = argv[2];
 
-  std::string input = "yt-dlp -x --audio-format wav -o " + title + " " + url;
+  std::string input = "yt-dlp -x --audio-format wav -o /Users/ryan/convertedMedia/" + title + " " + url;
 
   system(input.c_str());
   return 0;
-}
+} 
