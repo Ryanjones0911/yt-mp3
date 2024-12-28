0534532d1bd4e2fa6af69e400b0ddd55f394aa04
Ryan <65054422+Ryanjones0911@users.noreply.github.com>
2024-12-28 11:05:57 -0600

    altered main to both dynamically get user home path and to behave differently on Windows and Unix-based systems

--- main.cpp
+++ main.cpp
@@ -9,19 +9,20 @@
 
 int main (int argc, char *argv[]) 
 {
+  #ifdef _WIN32
+    const char* temp = std::getenv("USERPROFILE");
 
-  //create directory 'media' if it does not already exist. This is where all converted audio files will go
-  std::filesystem::path mediaPath = "/Users/ryan/YTRips";
-  if (!exists(mediaPath)) 
-  {
-    std::filesystem::create_directory(mediaPath);
-    printf("Directory created\n");
-  }
-  
+  #else 
+    //must be unix based
+    const char* temp = std::getenv("HOME");
+
+  #endif
+
+  std::string user_path = std::string(temp);
   std::string url = argv[1];
   std::string title = argv[2];
 
-  std::string input = "yt-dlp -x --audio-format wav -o /Users/ryan/YTRips/" + title + " " + url;
+  std::string input = "yt-dlp -x --audio-format wav -o '" + user_path + "/YTRips/'" + title + " " + url;
 
   system(input.c_str());
   return 0;
